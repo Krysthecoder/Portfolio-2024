f7d7637f855eda17661076bb45702bcc3c5490be
krysthopher@gmail.com <krysthopher@gmail.com>
2024-04-07 14:42:39 -0600

    added projects section, main styling as well as responsiveness, still need to create the copy writing as well as personalize the links and js funcitonality

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function relocate_home()
 {
-    location.href = "http://127.0.0.1:5500/index.html#About-me";
-     console.log('set')
+    location.href = "http://127.0.0.1:5500/index.html";
 } 
+
+function relocate_github() {
+    window.open('https://github.com/Krysthecoder', '_blank');
+}
